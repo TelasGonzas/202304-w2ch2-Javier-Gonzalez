7cf8d9ad599d4dc02bea5a5ce7e1ca724fc9d334
Javier González <jgestarriol@gmail.com>
2023-05-02 20:10:35 +0200

    pass second test

--- utils.test.js
+++ utils.test.js
@@ -11,15 +11,42 @@ describe("Create a strictEquals function", () => {
       expect(expectedValue).toBe(expectedResult);
     });
   });
-});
-
-describe("Create a strictEquals function", () => {
-  describe("When it receive two values (1,1)", () => {
+  describe("When it receive two values (NaN,NaN)", () => {
     test("It should return true", () => {
       const valueA = NaN;
       const valueB = NaN;
       const expectedValue = strictEquals(valueA, valueB);
+      const expectedResult = false;
+
+      expect(expectedValue).toBe(expectedResult);
+    });
+  });
+  describe("When it receive two values (0,-0)", () => {
+    test("It should return true", () => {
+      const valueA = 0;
+      const valueB = -0;
+      const expectedValue = strictEquals(valueA, valueB);
       const expectedResult = true;
+
+      expect(expectedValue).toBe(expectedResult);
+    });
+  });
+  describe("When it receive two values (-0,0)", () => {
+    test("It should return true", () => {
+      const valueA = -0;
+      const valueB = 0;
+      const expectedValue = strictEquals(valueA, valueB);
+      const expectedResult = true;
+
+      expect(expectedValue).toBe(expectedResult);
+    });
+  });
+  describe("When it receive two values (1,'1')", () => {
+    test("It should return true", () => {
+      const valueA = 1;
+      const valueB = "1";
+      const expectedValue = strictEquals(valueA, valueB);
+      const expectedResult = false;
 
       expect(expectedValue).toBe(expectedResult);
     });
