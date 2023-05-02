e0d28506d61dd48209c96e7c79e12a5cc27b7c06
Javier González <jgestarriol@gmail.com>
2023-05-02 19:52:25 +0200

    Second test

--- utils.test.js
+++ utils.test.js
@@ -12,3 +12,16 @@ describe("Create a strictEquals function", () => {
     });
   });
 });
+
+describe("Create a strictEquals function", () => {
+  describe("When it receive two values (1,1)", () => {
+    test("It should return true", () => {
+      const valueA = NaN;
+      const valueB = NaN;
+      const expectedValue = strictEquals(valueA, valueB);
+      const expectedResult = true;
+
+      expect(expectedValue).toBe(expectedResult);
+    });
+  });
+});
