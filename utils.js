6909e28079dd73e322663bc238c61e6226ed21f0
Javier González <jgestarriol@gmail.com>
2023-05-02 21:58:41 +0200

    Refactor +-0 condition

--- utils.js
+++ utils.js
@@ -1,13 +1,12 @@
 export const strictEquals = (valueA, valueB) => {
-  if (Object.is(valueA, NaN) || Object.is(valueB, NaN)) {
+  if (Object.is(valueA, NaN) && Object.is(valueB, NaN)) {
     return false;
   }
 
-  if (Object.is(valueA, 0) || Object.is(valueB, -0)) {
-    return true;
-  }
-
-  if (Object.is(valueA, -0) || Object.is(valueB, 0)) {
+  if (
+    (Object.is(valueA, 0) && Object.is(valueB, -0)) ||
+    (Object.is(valueA, -0) && Object.is(valueB, 0))
+  ) {
     return true;
   }
   return Object.is(valueA, valueB);
